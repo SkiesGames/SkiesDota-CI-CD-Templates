6dd7aaabcd8344e876e8b06fd672635bcad464dd
Egor Doroshuk <egor.doroshuk.04@mail.ru>
2025-06-30 06:09:31 +0300

    Add small fix

--- ansible/roles/mongodb/templates/init_replica_set.js
+++ ansible/roles/mongodb/templates/init_replica_set.js
@@ -1,8 +1,6 @@
 rs.initiate({
     _id: "rs0",
     members: [
-      { _id: 0, host: "{{ ansible_default_ipv4.address }}:27017", priority: 2 },
-      { _id: 1, host: "{{ ansible_default_ipv4.address }}:27018", priority: 1 },
-      { _id: 2, host: "{{ ansible_default_ipv4.address }}:27019", arbiterOnly: true }
+      { _id: 0, host: "{{ ansible_default_ipv4.address }}:27017", priority: 1 }
     ]
   }) 
